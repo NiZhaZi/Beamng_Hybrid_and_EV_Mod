70d4f00bef77a6b4fcf74d531d351d29e2ec85a8
NiZhaZi <e707886219@outlook.com>
2025-06-11 23:28:14 +0800

    wiper sound update

--- mod_package/hybrid/lua/controller/functions/wiper.lua
+++ mod_package/hybrid/lua/controller/functions/wiper.lua
@@ -1,26 +1,92 @@
+-- wiper.lua - 2025.6.10 - wiper control
+-- by NZZ
+-- version 0.0.2 alpha
+-- final edit - 2025.6.11 22:35
+
+-- Full files at https://github.com/NiZhaZi/Beamng_Hybrid_and_EV_Mod
+
 local M = {}
 
 local wiper = nil
 local wiperSpeed = nil
 local direction = 1
 
+local wiperSoundUp = nil
+local wiperSoundDown = nil
+local soundVolume = nil
+
 local function updateGFX(dt)
     if wiper and electrics.values.ignitionLevel == 2 then
         electrics.values.wiper = math.min(math.max(electrics.values.wiper + direction * wiperSpeed * dt, 0), 90)
         if electrics.values.wiper >= 90 or electrics.values.wiper <= 0 then
             direction = -direction
+            if direction == 1 then
+                obj:cutSFX(wiperSoundDown)
+            elseif direction == -1 then
+                obj:cutSFX(wiperSoundUp)
+            end
         end
     else
         electrics.values.wiper = math.max(electrics.values.wiper - 100 * dt, 0)
         wiperSpeed = 100
+        direction = 1
+    end
+
+    if wiper then
+        if direction == 1 then
+            obj:setVolume(wiperSoundUp, soundVolume)
+            obj:playSFX(wiperSoundUp)
+        elseif direction == -1 then
+            obj:setVolume(wiperSoundDown, soundVolume)
+            obj:playSFX(wiperSoundDown)
+        end
+    else
+        obj:setVolume(wiperSoundUp, 0)
+        obj:stopSFX(wiperSoundUp)
+
+        obj:setVolume(wiperSoundDown, 0)
+        obj:stopSFX(wiperSoundDown)
     end
 
 end
 
-local function init()
+local function init(jbeamData)
     wiper = false
     wiperSpeed = 100
     electrics.values.wiper = 0
+
+    local wiperSoundFile1 = "art/sound/wipers1.wav"
+    local wiperSoundFile2 = "art/sound/wipers2.wav"
+
+    for v, node in pairs (v.data.nodes) do	--for finding out the node ID
+		if node.name == jbeamData.nodeName then
+			wiperSoundUp = obj:createSFXSource(wiperSoundFile1, "AudioDefaultLoop3D", "", v)
+            wiperSoundDown = obj:createSFXSource(wiperSoundFile2, "AudioDefaultLoop3D", "", v)
+		end
+	end
+
+    soundVolume = jbeamData.soundVolume or 0.25
+end
+
+local function reset(jbeamData)
+    obj:setVolume(wiperSoundUp, 0)
+    obj:stopSFX(wiperSoundUp)
+    obj:setVolume(wiperSoundDown, 0)
+    obj:stopSFX(wiperSoundDown)
+
+    wiper = false
+    wiperSpeed = 100
+    electrics.values.wiper = 0
+
+    local wiperSoundFile1 = "art/sound/wipers1.wav"
+    local wiperSoundFile2 = "art/sound/wipers2.wav"
+
+    for v, node in pairs (v.data.nodes) do	--for finding out the node ID
+		if node.name == jbeamData.nodeName then
+			wiperSoundUp = obj:createSFXSource(wiperSoundFile1, "AudioDefaultLoop3D", "", v)
+            wiperSoundDown = obj:createSFXSource(wiperSoundFile2, "AudioDefaultLoop3D", "", v)
+		end
+	end
 end
 
 local function swicthWiper()
@@ -47,9 +113,9 @@ M.swicthWiper = swicthWiper
 M.setWiperSpeed = setWiperSpeed
 
 M.onInit = init
-M.onReset = init
+M.onReset = reset
 M.init = init
-M.reset = init
+M.reset = reset
 M.updateGFX = updateGFX
 
 return M
